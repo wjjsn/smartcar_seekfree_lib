a571b97dfd070f0694d001b1dfd1f2b8f1b045df
wjjsn <wjjsn@qq.com>
2026-06-11 07:20:56 +0000

    修复找不到opencv、找不到civetweb.h的问题

--- Example/cross.cmake
+++ Example/cross.cmake
@@ -15,7 +15,12 @@ set(CMAKE_RANLIB  ${TOOLCHAIN_DIR}/bin/loongarch64-linux-gnu-gcc-ranlib)
 SET(CMAKE_SYSROOT "${TOOLCHAIN_DIR}/loongarch64-linux-gnu/sysroot" CACHE INTERNAL "")
 
 # Search both toolchain sysroot and Buildroot target sysroot for headers/libraries/packages
-SET(CMAKE_FIND_ROOT_PATH ${TOOLCHAIN_DIR} ${CMAKE_SYSROOT} "/workspace/buildroot-2405/output/target")
+SET(CMAKE_FIND_ROOT_PATH 
+    ${TOOLCHAIN_DIR} 
+    ${CMAKE_SYSROOT} 
+    "/workspace/buildroot-2405/output/host/loongarch64-buildroot-linux-gnu/sysroot"
+    "/workspace/ls_2k0300_env/opencv_4_10_build"
+    )
 SET(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
 SET(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
 SET(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
@@ -25,5 +30,5 @@ SET(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -g3 -rdynamic -pthread -Wall -Wno-delete
 SET(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -march=loongarch64 -mtune=loongarch64 -fPIC -ffunction-sections -fdata-sections")
 SET(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -g3 -rdynamic -Wall -march=loongarch64 -mtune=loongarch64 -fPIC -ffunction-sections -fdata-sections")
 SET(CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS} -Wl,--gc-sections")
-
+find_package(OpenCV REQUIRED)
 ENDIF(CROSS_COMPILE)
